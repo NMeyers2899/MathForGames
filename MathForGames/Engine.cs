bb942101e3f55a4165a68b831858b8644949c619
nicholasMeyers <s218019@student.aie.edu>
2021-10-06 16:51:28 -0500

    Made a Scenes Class and a way to add Scenes

--- MathForGames/Engine.cs
+++ MathForGames/Engine.cs
@@ -6,7 +6,9 @@ namespace MathForGames
 {
     class Engine
     {
-        private static bool applicationShouldClose = false;
+        private static bool _applicationShouldClose = false;
+        private static int _currentSceneIndex;
+        private Scene[] _scenes = new Scene[0];
 
         /// <summary>
         /// Called to begin the application.
@@ -17,7 +19,7 @@ namespace MathForGames
             Start();
 
             // Loop until the application is told to close.
-            while (!applicationShouldClose)
+            while (!_applicationShouldClose)
             {
                 Update();
                 Draw();
@@ -31,25 +33,57 @@ namespace MathForGames
         /// </summary>
         private void Start()
         {
-
+            _scenes[_currentSceneIndex].Start();
         }
 
         /// <summary>
-        /// Loop until the application is told to close.
+        /// Called each time the game loops.
         /// </summary>
         private void Update()
         {
-
+            _scenes[_currentSceneIndex].Update();
         }
 
+        /// <summary>
+        /// Draws all necessary information to the screen.
+        /// </summary>
         private void Draw()
         {
-
+            _scenes[_currentSceneIndex].Draw();
         }
 
+        /// <summary>
+        /// Is called when the application is about to close.
+        /// </summary>
         private void End()
         {
+            _scenes[_currentSceneIndex].End();
+        }
 
+        /// <summary>
+        /// Adds a new scene to the _scenes array.
+        /// </summary>
+        /// <param name="scene"> The new scene being added to the array. </param>
+        /// <returns> The index that the new scene was added to. </returns>
+        public int AddScene(Scene scene)
+        {
+            // Creates a temporary array.
+            Scene[] tempArray = new Scene[_scenes.Length + 1];
+
+            // Copies all of the old values from the array and adds them to the new array.
+            for(int i = 0; i < _scenes.Length; i++)
+            {
+                tempArray[i] = _scenes[i];
+            }
+
+            // Sets the last index to be a new scene.
+            tempArray[_scenes.Length] = scene;
+
+            // Set the old array to the new array.
+            _scenes = tempArray;
+
+            // Return the last index.
+            return _scenes.Length - 1;
         }
     }
 }
