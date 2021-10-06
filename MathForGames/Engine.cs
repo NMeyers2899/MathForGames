d582d01c3ce95e65b4fc3f92f5ca46ce24646160
nicholasMeyers <s218019@student.aie.edu>
2021-10-06 16:37:15 -0500

    Made a Main Game Loop

---
+++ MathForGames/Engine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathForGames
+{
+    class Engine
+    {
+        private static bool applicationShouldClose = false;
+
+        /// <summary>
+        /// Called to begin the application.
+        /// </summary>
+        public void Run()
+        {
+            // Initalizes important variables for the application.
+            Start();
+
+            // Loop until the application is told to close.
+            while (!applicationShouldClose)
+            {
+                Update();
+                Draw();
+            }
+
+            End();
+        }
+
+        /// <summary>
+        /// Initalizes important variables for the application.
+        /// </summary>
+        private void Start()
+        {
+
+        }
+
+        /// <summary>
+        /// Loop until the application is told to close.
+        /// </summary>
+        private void Update()
+        {
+
+        }
+
+        private void Draw()
+        {
+
+        }
+
+        private void End()
+        {
+
+        }
+    }
+}
